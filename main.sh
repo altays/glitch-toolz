45b59731505d9a4c60637b0f0640df5ccb3efd82
Alex <altays633@gmail.com>
2025-11-27 22:07:31 -0500

    updating shell script:

---
+++ main.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+sampleJPG="IMG_3141.jpg"
+sampleJPGName="IMG_3141"
+sampleWav=""
+
+randomLoop=$[ ( $RANDOM % 9 )  + 2 ]
+
+if [ "$1" = "jpgAnalyze" ] 
+then
+    echo "route jpg analyze"
+    node index.js analyze $sampleJPG
+    cp -r assets/data/$sampleJPGName assets/data/$sampleJPGName-bending
+elif [ "$1" = "jpgCompile" ]
+then
+    echo "route jpg compile" 
+    node index.js compile IMG_3141-bending jpg
+elif [ "$1" = "i" ]
+then
+    echo "route initialize" 
+    mkdir assets assets/data assets/input assets/output
+else
+    echo "Please indicate a valid route."
+fi
